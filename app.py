317397cf86fcb70c3e5d39bb84b103c4040398ee
ryannb21 <162481285+ryannb21@users.noreply.github.com>
2025-07-12 15:51:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ if email_secrets is None:
 # Gmail SMTP configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = email_secrets['MAIL_USERNAME']         # replace this
-app.config['MAIL_PASSWORD'] = email_secrets['MAIL_PASSWORD']      # use Gmail app password, NOT regular password
+app.config['MAIL_USERNAME'] = email_secrets['email']         # replace this
+app.config['MAIL_PASSWORD'] = email_secrets['password']      # use Gmail app password, NOT regular password
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
